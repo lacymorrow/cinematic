22719a860162b90d9a98e28abb49ab407b1212d6
Lacy Morrow <me@lacymorrow.com>
2019-10-30 13:59:25 -0700

    Modularize templates

---
+++ imports/ui/index.js
@@ -0,0 +1,19 @@
+import {bullets} from './bullets'
+import {details} from './details'
+import {directory} from './directory'
+import {display} from './display'
+import {header} from './header'
+import {movies} from './movies'
+import {sidebar} from './sidebar'
+import {sort} from './sort'
+
+export {
+	bullets,
+	details,
+	directory,
+	display,
+	header,
+	movies,
+	sidebar,
+	sort
+}
