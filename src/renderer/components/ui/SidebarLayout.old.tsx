839d3e59a5343d67795e18a43cbbf43f3224c039
Lacy Morrow <me@lacymorrow.com>
2023-12-31 21:20:16 -0500

    remove sidebar

--- src/renderer/components/ui/SidebarLayout.old.tsx
+++ src/renderer/components/ui/SidebarLayout.old.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Sidebar } from './Sidebar';
+import { Sidebar } from './Sidebar.old';
 
 export function SidebarLayout({ children }: { children: React.ReactNode }) {
 	return (
