dc647abdb4ecd29328c5ff10ec86d06bcb6b0a27
Lacy Morrow <me@lacymorrow.com>
2026-03-22 03:38:39 -0400

    auto commit

--- src/renderer/context/global-context.tsx
+++ src/renderer/context/global-context.tsx
@@ -110,7 +110,7 @@ export function GlobalContextProvider({
 				// Setup listener to play sounds
 				window.electron.ipcRenderer.on(ipcChannels.PLAY_SOUND, (sound: any) => {
 					if (!settings.allowSounds) return;
-					play({ name: sound });
+					play({ name: sound, path: '' });
 				});
 			})
 			.catch(console.error);
